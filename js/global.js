ced26bb89beea485096d1e4231586568b0260f54
Jason Nardone <jlnardone@gmail.com>
2014-12-01 11:56:10 -0500

    fixed typo listing software twice.

--- js/global.js
+++ js/global.js
@@ -1,2 +1,2 @@
 // @codekit-append "../bower_components/jquery/dist/jquery.js"
-// @codekit-append "../bower_components/bootstrap/js/modal.js"
+// @codekit-append "../bower_components/bootstrap/dist/js/bootstrap.js"
